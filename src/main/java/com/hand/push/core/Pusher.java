f74bb529f549ea635f5db078d8cbe72253c4c983
Emerson Zhang <zhemerson@gmail.com>
2013-12-27 14:26:24 +0800

    重命名配置文件

--- src/main/java/com/hand/push/core/Pusher.java
+++ src/main/java/com/hand/push/core/Pusher.java
@@ -12,7 +12,7 @@ import java.util.List;
  * User: emerson
  * Date: 10/14/13
  * Time: 9:54 AM
- * To change this template use File | Settings | File Templates.
+ *
  */
 public interface Pusher {
     /**
